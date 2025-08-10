37520843df72893e163f600baa9f6f702115e5cd
Brice Lenfant <165644+bricel@users.noreply.github.com>
2025-08-10 13:21:30 +0300

    lithbox

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     initSlideshow();
     initSmoothScrolling();
     initAnimations();
+    initLightbox();
     
     /**
      * Navigation functionality
